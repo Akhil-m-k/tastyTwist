a7014ea4cb343f5e8a7cc85f7c9fc5d9f8442f43
Akhil mk <123466565+Akhil-m-k@users.noreply.github.com>
2024-03-09 19:13:49 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ const Login = ()=>{
       }, []);
     return (
   <div className="flex flex-col items-center justify-center min-h-screen px-6  mx-auto  pt-28 lg:py-0">
-      <div className="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+      <div className="w-full bg-white rounded-lg shadow-custom-sm dark:border md:mt-0 sm:max-w-md xl:p-0 ">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                   Sign in to your account
