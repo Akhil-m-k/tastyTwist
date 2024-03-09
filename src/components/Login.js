b200afd6d32a018221f147c616fa0c879e5d4cc1
Akhil mk <123466565+Akhil-m-k@users.noreply.github.com>
2024-03-09 18:45:48 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
+import {useEffect} from "react";
+
 const Login = ()=>{
+    useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
     return (
-  <div className="flex flex-col items-center justify-center px-6 mx-auto  min-h-screen  pt-28 lg:py-0">
+  <div className="flex flex-col items-center justify-center min-h-screen px-6  mx-auto  pt-28 lg:py-0">
       <div className="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 ">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
