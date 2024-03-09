37f27945d9b6f78d89e6ce1cb635eb264d798dd6
Akhil mk <123466565+Akhil-m-k@users.noreply.github.com>
2024-03-09 17:34:59 +0530

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const SignUp = ()=>{
     return (
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  min-h-screen  pt-28 lg:py-0">
+  <div className="flex flex-col items-center justify-center px-6  mx-auto  min-h-screen  pt-28 lg:py-0">
       <div className="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 ">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
