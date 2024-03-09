58f4d5cba02aa71ad007d65b4a6fd3b31addd8f7
Akhil mk <123466565+Akhil-m-k@users.noreply.github.com>
2024-03-09 18:46:55 +0530

    Update RestaurantMenu.js

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -5,20 +5,22 @@ import { useDispatch } from "react-redux";
 import { IMG_Restauro_Card, ITEM_IMG_CDN_URL } from "../config";
 import dummyImg from "../../public/images/dummy.png";
 import MenuSkeleton from "./menuSkeleton";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 const Section = ({ restauro, title, identifier, isVisible, setVisible }) => {
   const dispatch = useDispatch();
   const addFoodItem = (item) => {
     dispatch(addItem(item));
   };
-
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div key={identifier}>
       {isVisible ? (
         <div
           key={identifier + 1}
-          className="cursor-pointer text-2xl flex justify-between font-medium bg-gray-200 p-3 mb-2"
+          className="cursor-pointer text-lg sm:text-2xl flex justify-between font-medium bg-white shadow-md p-3 mb-2"
           onClick={() => setVisible(false)}
         >
           <span>{title}</span>
@@ -29,7 +31,7 @@ const Section = ({ restauro, title, identifier, isVisible, setVisible }) => {
       ) : (
         <div
           key={identifier + 2}
-          className="cursor-pointer flex justify-between text-2xl font-medium bg-gray-200 p-3 mb-2"
+          className="cursor-pointer flex justify-between text-lg sm:text-2xl font-medium bg-white shadow-md p-3 mb-2"
           onClick={() => setVisible(true)}
         >
           <span>{title}</span>
@@ -41,23 +43,23 @@ const Section = ({ restauro, title, identifier, isVisible, setVisible }) => {
       {isVisible && (
         <div key={identifier + 3}>
           {restauro.itemCards.map((item, index) => (
-            <div className="item-card flex flex-col sm:flex-row border-b-2 py-4" key={index + identifier}>
+            <div className="item-card flex flex-col sm:flex-row border-b-2 border-gray-300 py-4" key={index + identifier}>
               <div className="pr-3 max-w-28 flex flex-col items-center justify-center  mt-2 sm:mt-0">
                 <img
                   src={item.card.info.imageId ? ITEM_IMG_CDN_URL + item.card.info.imageId : dummyImg}
                   alt=""
-                  className="rounded-md min-h-20 w-full"
+                  className="rounded-md min-h-20 w-full border border-gray-300"
                 />
                 <button
-                  className="bg-green-600 px-4 rounded-md text-white shadow w-full p-1 mt-2"
+                  className="bg-green-600 px-4 rounded-md text-white shadow w-full mb-2 sm:mb-0 p-1 mt-2"
                   onClick={() => addFoodItem(item.card.info)}
                 >
                   Add
                 </button>
               </div>
               <div className="">
-                <h3 className="text-2xl font-bold">{item.card.info.name}</h3>
-                <h5 className="text-gray-500">{item.card.info.description}</h5>
+                <h3 className=" text-xl sm:text-2xl font-bold mb-2 sm:mb-0">{item.card.info.name}</h3>
+                <h5 className="text-gray-500 mb-2 sm:mb-0">{item.card.info.description}</h5>
                 <h4 className="text-lg  font-bold bg-green-100 inline px-2">
                   {"₹" + Math.ceil(item.card.info.defaultPrice ? item.card.info.defaultPrice / 100 : item.card.info.price / 100)}
                 </h4>
@@ -138,7 +140,7 @@ const RestaurantMenu = () => {
             );
           } else if (restauro.card.card.categories) {
             return (
-              <React.Fragment key={restauro.card.card.title}>
+              <div key={restauro.card.card.title}>
                 {restauro.card.card.categories.map((category, index) => (
                   <Section
                     key={category.title}
@@ -149,7 +151,7 @@ const RestaurantMenu = () => {
                     setVisible={(bool) => bool ? setIsVisibleSection(category.title) : setIsVisibleSection("")}
                   />
                 ))}
-              </React.Fragment>
+              </div>
             );
           }
           return null;
